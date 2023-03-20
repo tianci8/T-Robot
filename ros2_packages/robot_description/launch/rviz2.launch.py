837f5f5531336982249c649833cc9644fb01ca4c
z2010012286 <tianci_zhang@tju.edu.cn>
2023-03-20 14:10:23 +0800

    show TROBOT in rviz2

---
+++ ros2_packages/robot_description/launch/rviz2.launch.py
@@ -0,0 +1,86 @@
+'''
+Author: Tianci Zhang
+Email: tianci_zhang@tju.edu.cn
+Date: 2023-01-27 21:20:51
+LastEditors: Tianci Zhang
+LastEditTime: 2023-01-27 21:22:26
+FilePath: \T-Robot\ros2_packages\robot_description\launch\description.launch.py
+Description: 
+
+Copyright (c) 2023 by tianci_zhang@tju.edu.cn, All Rights Reserved. 
+'''
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, EnvironmentVariable
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    urdf_path = PathJoinSubstitution(
+        [FindPackageShare("robot_description"), "urdf/trobot_rviz2", "trobot.urdf.xacro"]
+    )
+    rviz_config_path=PathJoinSubstitution(
+        [FindPackageShare("robot_description"), "rviz", "trobot_rviz2_show.rviz"]
+    )
+#使用ziji
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='urdf', 
+            default_value=urdf_path,
+            description='URDF path'
+        ),
+        
+        DeclareLaunchArgument(
+            name='publish_joints', 
+            default_value='true',
+            description='Launch joint_states_publisher'
+        ),
+
+        DeclareLaunchArgument(
+            name='rviz', 
+            default_value='true',
+            description='Run rviz'
+        ),
+
+        DeclareLaunchArgument(
+            name='use_sim_time', 
+            default_value='false',
+            description='Use simulation time'
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            condition=IfCondition(LaunchConfiguration("publish_joints"))
+            # parameters=[
+            #     {'use_sim_time': LaunchConfiguration('use_sim_time')}
+            # ] #since galactic use_sim_time gets passed somewhere and rejects this when defined from launch file
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                    'robot_description': Command(['xacro ', LaunchConfiguration('urdf')])
+                }
+            ]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            condition=IfCondition(LaunchConfiguration("rviz")),
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        )
+    ])
